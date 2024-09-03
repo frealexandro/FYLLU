e073fa3a21507c1a753c25ee26de9f5d6c448cd5
frealexandro <jhoner300@gmail.com>
2024-09-02 22:07:22 -0500

    we make the build to deploy in vercel

---
+++ fronted/fronted/state/PageState.py
@@ -0,0 +1,34 @@
+# import reflex as rx
+# import fronted.utils as utils
+# from link_bio.api.api import live, featured, schedule
+# from link_bio.model.Featured import Featured
+# from link_bio.model.Live import Live
+
+# USER = "mouredev"
+
+
+# class PageState(rx.State):
+
+#     timezone = ""
+#     live_status = Live(live=False, title="")
+#     next_live = ""
+#     featured_info: list[Featured]
+
+#     async def check_live(self):
+#         self.live_status = await live(USER)
+
+#     async def check_schedule(self):
+#         if self.timezone == "":
+#             return rx.call_script(
+#                 utils.LOCAL_TIMEZONE_SCRIPT,
+#                 PageState.update_timezone
+#             )
+#         else:
+#             await self.update_timezone(self.timezone)
+
+#     async def update_timezone(self, timezone: str):
+#         self.timezone = timezone
+#         self.next_live = utils.next_date(await schedule(), self.timezone)
+
+#     async def featured_links(self):
+#         self.featured_info = await featured()
--- fronted/local_build.sh
+++ fronted/local_build.sh
@@ -3,7 +3,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=https://api.randia.io reflex export --frontend-only
+reflex export --frontend-only
 reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
