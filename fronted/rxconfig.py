ec3853f5d899e444161227b90b12b69c16b39c4d
frealexandro <jhoner300@gmail.com>
2024-09-05 16:36:19 -0500

    all the people can conect

--- fronted/rxconfig.py
+++ fronted/rxconfig.py
@@ -2,12 +2,11 @@ import reflex as rx
 
 config = rx.Config(
     app_name="fronted",
-    cors_allowed_origins = [
-        "http://localhost:3000",
-        "https://randia.io",
-        "https://rand-7x2s0oi2l-santiagos-projects-14c51bdf.vercel.app",
-        
-    ],
+    #cors_allowed_origins = [
+    #    "http://localhost:3000",
+    #    "https://www.randia.io",
+    #    
+    #],
     tailwind={
         "theme": {
             "extend": {},
