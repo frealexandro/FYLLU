7fe758f73032a7490c6035d3b7e201848cd95202
frealexandro <jhoner300@gmail.com>
2024-09-04 22:18:24 -0500

    final deploy

--- fronted/remote_build.sh
+++ fronted/remote_build.sh
@@ -3,6 +3,7 @@ python -m venv .env
 source .env/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+#requirements
 rm -rf public
 reflex init
 API_URL=https://api.randia.io reflex export --frontend-only
