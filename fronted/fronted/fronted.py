e5fe533384f0933db5a0080ee8766563d76e52ed
frealexandro <jhoner300@gmail.com>
2024-09-04 23:46:00 -0500

    we fix the error

--- fronted/fronted/fronted.py
+++ fronted/fronted/fronted.py
@@ -4,16 +4,14 @@ import reflex as rx
 from fronted.pages.index import index
 from fronted.pages.funnel_ventas import funnel_ventas
 import fronted.styles.styles as styles
+from fronted.api.api import featured
 #from fronted.api import api
 
-
-
-class State(rx.State):
-    """define the state of the app"""
 
 
 # Create app instance and add index page.
 # Create app instance and add index page.
 app = rx.App()
-app.add_page(index)
-app.add_page(funnel_ventas)
+#app.add_page(index)
+#app.add_page(funnel_ventas)
+app.api.add_api_route("/featured", featured)
