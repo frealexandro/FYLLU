87e43de10e4d1e47c34a99cde68c189d50f4192d
frealexandro <jhoner300@gmail.com>
2024-09-05 19:32:36 -0500

    we will deploy the app in gcp

--- fronted/rxconfig.py
+++ fronted/rxconfig.py
@@ -2,11 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="fronted",
-    #cors_allowed_origins = [
-    #    "http://localhost:3000",
-    #    "https://www.randia.io",
-    #    
-    #],
     tailwind={
         "theme": {
             "extend": {},
